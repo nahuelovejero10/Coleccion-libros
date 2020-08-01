ebaac47650b211b5847253f29a02bb16c2b1eff1
nahuel <nahuel_ovejero@outlook.es>
2020-08-01 00:01:39 -0300

    Agregando libros, limpiando campos y traer libros

---
+++ app.js
@@ -0,0 +1,73 @@
+//DEFINICION DE LAS CLASES
+class Libro{
+    constructor(titulo, autor, isbn){
+        this.titulo = titulo
+        this.autor = autor
+        this.isbn = isbn
+    }
+}
+
+class UI{
+
+    static mostrarLibros(){}
+    static agregarLibro(libro){}
+    static eliminarLibro(){}
+
+    static mostrarAlerta(mensaje, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(mensaje));
+
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#libro-form');
+        container.insertBefore(div, form);
+
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+    static limpiarCampos(){
+        document.querySelector('#titulo').value = '';
+        document.querySelector('#autor').value = '';
+        document.querySelector('#isbn').value = '';
+    }
+
+}
+
+class Datos{
+
+    static traerLibros(){
+        let libros;
+        if(localStorage.getItem('libros') === null){
+            libros = [];            
+        }else{
+            libros = JSON.parse(localStorage.getItem('libros'));
+        }
+        return libros;
+    }
+
+    static agregarUnLibro(libro){
+        const libros = Datos.traerLibros();
+        libros.push(libro);
+        localStorage.setItem('libros',JSON.stringify(libros));
+        
+    }
+    static removerLibro(isbn){}
+
+}
+
+//Controlar el evento submit
+document.querySelector('#libro-form').addEventListener('submit',(e) => {
+e.preventDefault();
+
+//Obtener valores de los campos
+const titulo = document.querySelector('#titulo').value;
+const autor = document.querySelector('#autor').value;
+const isbn = document.querySelector('#isbn').value;
+
+if(titulo === '' || autor === '' || isbn === ''){
+    UI.mostrarAlerta('Por favor ingrese todos los datos','danger');
+}else{
+    const libro = new Libro(titulo, autor, isbn);
+    Datos.agregarUnLibro(libro);
+    UI.limpiarCampos();
+}
+});
